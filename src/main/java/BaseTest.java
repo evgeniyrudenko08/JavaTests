1e1083b80a77b30f9143d5efafaea56c77fe3fd5
Evgeniy Rudenko <Evgeniy.Rudenko@aareon.com>
2019-07-28 11:08:16 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- src/main/java/BaseTest.java
+++ src/main/java/BaseTest.java
@@ -1,8 +1,11 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
@@ -11,10 +14,23 @@ public class BaseTest {
     public Page page;
 
     @BeforeMethod
-    public void setUp(){
-
+    @Parameters(value={"browser"})
+    public void setUpTest(String browser){
+        if (browser.equals("chrome")){
+            WebDriverManager.chromedriver().setup();
+            System.setProperty("webdriver.chrome.driver", "C:\\Users\\evgeniy.rudenko\\Downloads\\chromedriver_win32\\chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+        else if (browser.equals("ff")){
+            WebDriverManager.firefoxdriver().setup();
+            System.setProperty("webdriver.gecko.driver", "C:\\Users\\evgeniy.rudenko\\Downloads\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+            driver = new FirefoxDriver();
+        }
+        else {
+            System.out.println("no browser is defined in xml file");
+        }
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\evgeniy.rudenko\\Downloads\\chromedriver_win32\\chromedriver.exe");
-        driver = new ChromeDriver();
+
         wait = new WebDriverWait(driver, 15);
         driver.manage().window().fullscreen();
         driver.get("http://users.bugred.ru/user/login/index.html");
--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -1,10 +1,15 @@
+import org.openqa.selenium.Cookie;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LoginPageTest extends BaseTest{
+public class LoginPageTest extends BaseTest {
 
 
     @Test(priority = 1)
-    public void loginTest(){
+    public void loginTest() {
         page.getInstance(LoginPage.class).doLogin("test780@test50.com", "123456q11");
+        String cookieOfRegistration = "misterti";
+        boolean cookie = driver.manage().getCookies().toString().contains(cookieOfRegistration);
+        Assert.assertTrue(cookie);
     }
 }
