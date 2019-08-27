f9c7b58ef1e621b0dd6fe22347b2902085589584
Evgeniy Rudenko <Evgeniy.Rudenko@aareon.com>
2019-08-27 13:03:25 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- src/main/java/Core/BaseUITestInSauceLabs.java
+++ src/main/java/Core/BaseUITestInSauceLabs.java
@@ -1,11 +1,9 @@
 package Core;
 
 import com.codeborne.selenide.Configuration;
-import org.testng.annotations.BeforeClass;
 
 
 public class BaseUITestInSauceLabs {
-    @BeforeClass
     public void setUp() {
         Configuration.baseUrl = "http://167.71.169.4";
         Configuration.browser = "Core.RemoteWebDriverProvider";
